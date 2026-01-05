3a392b9dfc9915dbfcdbe23b2fc6f99ddff114b0
HGF50 <haccandypaulo@gmail.com>
2026-01-05 12:26:52 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,37 @@
+const CACHE_NAME = "drop-ton-weh-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("pwa-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
@@ -17,3 +41,5 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
+
